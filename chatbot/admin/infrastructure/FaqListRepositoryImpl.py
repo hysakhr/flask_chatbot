24481e71d9e782d927c138f34913a839d1be6034
hysakhr <hysakhr@gmail.com>
2019-09-27 16:18:26 +0900

    select文を db.sessionの記述に統一

--- chatbot/admin/infrastructure/FaqListRepositoryImpl.py
+++ chatbot/admin/infrastructure/FaqListRepositoryImpl.py
@@ -5,7 +5,7 @@ from chatbot.database import db
 
 class FaqListRepositoryImpl(IFaqListRepository):
     def find_by_id(self, id: int) -> FaqListModel:
-        return FaqListModel.query.get(id)
+        return db.session.query(FaqListModel).get(id)
 
     def find_by_name(self, name: str) -> FaqListModel:
         pass
@@ -15,5 +15,5 @@ class FaqListRepositoryImpl(IFaqListRepository):
         return db.session.commit()
 
     def get_list(self) -> list:
-        return FaqListModel.query.order_by(FaqListModel.id).all()
-        # return db.session.query(FaqListModel).all()
+        return db.session.query(FaqListModel).order_by(
+            FaqListModel.updated_at.desc()).all()
