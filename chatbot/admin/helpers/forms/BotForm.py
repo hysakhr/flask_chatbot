af48e7e5c016117939042502460f9a95740e02fc
hysakhr <hysakhr@gmail.com>
2019-10-20 10:24:31 +0900

    fix validation not working(bot, site)

--- chatbot/admin/helpers/forms/BotForm.py
+++ chatbot/admin/helpers/forms/BotForm.py
@@ -6,7 +6,7 @@ class BotForm(FlaskForm):
     name = StringField('BOT名')
     enable_flag = BooleanField('有効 / 無効')
 
-    def validation_name(self, name):
+    def validate_name(self, name):
         """バリデーション
 
         - 必須
@@ -15,5 +15,5 @@ class BotForm(FlaskForm):
         if name.data == '':
             raise ValidationError('必須です。')
 
-        if len(name.date) > 255:
+        if len(name.data) > 255:
             raise ValidationError('255文字以内で入力してください。')
