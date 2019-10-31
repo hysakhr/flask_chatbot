b88551761403e3f02beb6ea1187875c1e41eb106
hysakhr <hysakhr@gmail.com>
2019-11-01 08:54:05 +0900

    when docker-compose up, run 'flask db upgrade'

--- start.sh
+++ start.sh
@@ -4,6 +4,6 @@ cd /flask_chatbot/chatbot
 npm run watch &
 
 cd /flask_chatbot
-# flask db upgrade
+flask db upgrade
 celery -A chatbot.celery_worker.celery worker -l info &
 flask run --port 5000 --host 0.0.0.0
